bd6883cb5fe951dafc2fa553e843c5ebdc917356
theclearwindow <omar.beuchat@gmail.com>
2020-03-03 15:31:59 +0100

    fakename whatever

--- Assets/Scripts/Chase.cs
+++ Assets/Scripts/Chase.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Chase : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform target;
+    Vector3 destination;
+    NavMeshAgent DeeDee;
     void Start()
     {
-        
+        DeeDee = GetComponent<NavMeshAgent>();
+        destination = DeeDee.destination;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(destination, target.position) > 1.0f)
+        {
+            destination = target.position;
+            DeeDee.destination = destination;
+        }
     }
 }
